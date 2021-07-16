6972324941f29c7072720da7932a7e9c12ba00e8
Pani <spani@dresources.com>
2021-07-16 11:41:00 +0530

    first commit

---
+++ src/test/java/Demo.java
@@ -0,0 +1,40 @@
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("this is first test");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("this is second test");
+	}
+
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("this is third test");
+	}
+
+	
+	
+	@Test
+	public void test4()
+	{
+		System.out.println("this is fourth test");
+	}
+
+	
+	@Test
+	public void tes5()
+	{
+		System.out.println("this is fivfth test");
+	}
+
+
+}
